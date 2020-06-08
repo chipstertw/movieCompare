55ab258b3df6db8e1fbe1430ec97aeb1d7be7975
Yichi <62705589+chipstertw@users.noreply.github.com>
2020-06-08 11:02:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const autocompleteConfig = {
 		return movie.Title;
 	},
 	async fetchData(searchTerm) {
-		const response = await axios.get('http://www.omdbapi.com', {
+		const response = await axios.get('https://www.omdbapi.com', {
 			params: {
 				apikey: '73181c04',
 				//always lower case for apikey
